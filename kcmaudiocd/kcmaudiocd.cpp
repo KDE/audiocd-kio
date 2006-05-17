b63f56ba082169a7815bbea8887e947fb29fcacc
Chris Howells <howells@kde.org>
2006-05-17 00:13:28 +0000

    forward port 541685
    CCBUG:119962
    
    svn path=/trunk/KDE/kdemultimedia/kioslave/audiocd/; revision=541689

--- kcmaudiocd/kcmaudiocd.cpp
+++ kcmaudiocd/kcmaudiocd.cpp
@@ -72,6 +72,7 @@ KAudiocdModule::KAudiocdModule(KInstance *instance, QWidget *parent, const QStri
     connect(audioConfig->ec_enable_check,SIGNAL(clicked()),this,SLOT(slotEcEnable()));
     connect(audioConfig->ec_skip_check,SIGNAL(clicked()),SLOT(slotConfigChanged()));
     connect(audioConfig->cd_device_string,SIGNAL(textChanged(const QString &)),SLOT(slotConfigChanged()));
+    connect(niceLevel,SIGNAL(valueChanged(int)),SLOT(slotConfigChanged()));
 
     // File Name
     connect(audioConfig->fileNameLineEdit, SIGNAL(textChanged(const QString &)), this, SLOT(slotConfigChanged()));
