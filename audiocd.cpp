00e9f16fd45ab75bd586a83d206e8575770f836e
David Faure <faure@kde.org>
2008-10-16 15:39:05 +0000

    Move kio_audiocd-specific configure check to kio_audiocd
    svn path=/trunk/KDE/kdemultimedia/kioslave/audiocd/; revision=872204

--- audiocd.cpp
+++ audiocd.cpp
@@ -23,6 +23,7 @@
  */
 
 #include <config.h>
+#include <config-audiocd.h>
 
 #include <kdemacros.h>
 extern "C"
---
+++ config-audiocd.h.cmake
@@ -0,0 +1,3 @@
+/* Define if your cdda has a struct cdrom_drive member ioctl_device_interface */
+#cmakedefine HAVE_CDDA_IOCTL_DEVICE 1
+
