adf966bce090dee726f6752968d0532236d45fd0
Friedrich W. H. Kossebau <kossebau@kde.org>
2021-05-17 02:38:27 +0200

    Use more target-centric cmake code
    GIT_SILENT

--- cmake/FindFLAC.cmake
+++ cmake/FindFLAC.cmake
@@ -8,7 +8,7 @@
 if(FLAC_INCLUDE_DIR)
     # Already in cache, be silent
     set(FLAC_FIND_QUIETLY TRUE)
-endif(FLAC_INCLUDE_DIR)
+endif()
 
 find_path(FLAC_INCLUDE_DIR FLAC/stream_decoder.h)
 
@@ -20,16 +20,38 @@ find_library(OGG_LIBRARY NAMES ogg ogg_static libogg libogg_static)
 # Handle the QUIETLY and REQUIRED arguments and set FLAC_FOUND to TRUE if
 # all listed variables are TRUE.
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(FLAC DEFAULT_MSG
-    FLAC_INCLUDE_DIR OGG_LIBRARY FLAC_LIBRARY)
+find_package_handle_standard_args(FLAC
+    DEFAULT_MSG
+    FLAC_INCLUDE_DIR OGG_LIBRARY FLAC_LIBRARY
+)
 
 if(FLAC_FOUND)
-  set(FLAC_LIBRARIES ${FLAC_LIBRARY} ${OGG_LIBRARY})
-  if(WIN32)
-    set(FLAC_LIBRARIES ${FLAC_LIBRARIES} wsock32)
-  endif(WIN32)
-else(FLAC_FOUND)
-  set(FLAC_LIBRARIES)
-endif(FLAC_FOUND)
+    set(FLAC_LIBRARIES ${FLAC_LIBRARY} ${OGG_LIBRARY})
+    if(WIN32)
+        set(FLAC_LIBRARIES ${FLAC_LIBRARIES} wsock32)
+    endif()
+else()
+    set(FLAC_LIBRARIES)
+endif()
 
-mark_as_advanced(FLAC_INCLUDE_DIR FLAC_LIBRARY)
+mark_as_advanced(
+    FLAC_INCLUDE_DIR
+    FLAC_LIBRARY
+)
+
+if(FLAC_FOUND AND NOT TARGET FLAC::FLAC)
+    add_library(FLAC::FLAC UNKNOWN IMPORTED)
+    set_target_properties(FLAC::FLAC PROPERTIES
+        IMPORTED_LOCATION "${FLAC_LIBRARY}"
+        INTERFACE_INCLUDE_DIRECTORIES "${FLAC_INCLUDE_DIR}"
+    )
+    if(WIN32)
+        set_target_properties(FLAC::FLAC PROPERTIES
+            INTERFACE_LINK_LIBRARIES  "${OGG_LIBRARY};wsock32"
+        )
+    else()
+        set_target_properties(FLAC::FLAC PROPERTIES
+            INTERFACE_LINK_LIBRARIES  "${OGG_LIBRARY}"
+        )
+    endif()
+endif()
